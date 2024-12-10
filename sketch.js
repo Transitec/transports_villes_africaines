734286110bf7473257fd46842bc60cf461567afb
adescombes <albane@descombes.org>
2024-12-10 10:43:37 +0100

    map working fine with simple popups and images

--- sketch.js
+++ sketch.js
@@ -7,44 +7,59 @@ const attr = '&copy; <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © 
 const map = L.map('mapid').setView([ 30.0618851, -1.9440727 ], 5);
 
 
-// L.geoJSON(villes_africaines).addTo(map);
 
- // Charger le fichier GeoJSON en local
- fetch('villes_africaines_test.geojson') // Remplacez par le chemin de votre fichier GeoJSON
- .then(response => response.json())  // Convertir la réponse en JSON
- .then(data => {
-   // Charger les données GeoJSON sur la carte
-   L.geoJSON(data, {
-    onEachFeature: function (feature, layer) {
-      // Vérifier si la feature a une géométrie de type Point
-      if (feature.geometry && feature.geometry.type === 'Point') {
-        var latlng = [feature.geometry.coordinates[1], feature.geometry.coordinates[0]]; // [lat, lng]
 
-        // Créer un marker pour chaque feature
-        var marker = L.marker(latlng).addTo(map);
+fetch('villes_africaines_test.geojson')
+  .then(response => response.json())
+  .then(data => {
 
-        // Créer un contenu personnalisé pour le popup
-        var popupContent = "<strong>" + feature.properties.name + "</strong>";
+    L.geoJSON(data, {
+      onEachFeature: function (feature, layer) {
 
-        // Lier le popup au marker
-        marker.bindPopup(popupContent, {
-          maxWidth: 300,  // Largeur maximale du popup
-          closeOnClick: false  // Ne ferme pas le popup lorsque l'on clique ailleurs
+        var popupContent = '<h1>'+feature.properties.name+'</h1><img src="./img/'+feature.properties.name+'.png" style="max-width: 100%; height: auto;">'
+        
+        var latlng = [feature.geometry.coordinates[1], feature.geometry.coordinates[0]];
+
+        layer.bindPopup(popupContent, {
+          maxWidth: "auto", 
+          minWidth: 300,
         });
-      }
-    }
-  }).addTo(map);
-})
- .catch(error => {
-   console.error("Erreur lors du chargement du fichier GeoJSON:", error);
- });
 
+      }
+    }).addTo(map);
+  })
+
+
+/* fetch('villes_africaines_test.geojson')
+  .then(response => response.json())
+  .then(data => {
+    L.geoJSON(data, {
+      onEachFeature: function (feature, layer) {
+
+      if (feature.geometry && feature.geometry.type === 'Point') {
+          var latlng = [feature.geometry.coordinates[1], feature.geometry.coordinates[0]]; // [lat, lng]
+
+          var marker = layer.marker(latlng).addTo(map);
+
+          var popupContent = "<strong>" + feature.properties.name + "</strong><br>" 
+
+          marker.bindPopup(popupContent);
+        }
+      }
+    }).addTo(map);
+  })
+  .catch(error => {
+    console.error("Erreur lors du chargement du fichier GeoJSON:", error);
+  }); */
 
 
 
 const tile = L.tileLayer(mapboxUrl, {
   attribution: attr
 }).addTo(map);
+
+
+
 
 
 
