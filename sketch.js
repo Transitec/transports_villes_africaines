a866c59e4ac0f43143132b21f1e3de2b4c7b8567
adescombes <albane@descombes.org>
2024-12-10 11:47:51 +0100

    map working with custom colors and images' tight layout

--- sketch.js
+++ sketch.js
@@ -7,55 +7,45 @@ const attr = '&copy; <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © 
 const map = L.map('mapid').setView([ 30.0618851, -1.9440727 ], 5);
 
 
-
-
-fetch('villes_africaines_test.geojson')
+fetch('villes_africaines.geojson')
   .then(response => response.json())
   .then(data => {
 
-    L.geoJSON(data, {
-      onEachFeature: function (feature, layer) {
+L.geoJSON(data, {
 
-        var popupContent = '<h1>'+feature.properties.name+'</h1><img src="./img/'+feature.properties.name+'.png" style="max-width: 100%; height: auto;">'
-        
-        var latlng = [feature.geometry.coordinates[1], feature.geometry.coordinates[0]];
+    pointToLayer: function (feature, latlng, layer) {
 
-        layer.bindPopup(popupContent, {
-          maxWidth: "auto", 
-          minWidth: 300,
+        var color = feature.properties.color;
+
+            return L.circleMarker(latlng, {
+                radius: 8,
+                fillColor: color,
+                color: 'white',
+                weight: 1,
+                opacity: 0.4,
+                fillOpacity: 0.4
+            });
+    },
+
+    onEachFeature: function (feature, layer) {
+
+          var popupContent = '<h1>'+feature.properties.name+'</h1><img src="'+feature.properties.img_path+'"style="max-width: 100%; height: auto;">'
+          
+          layer.bindPopup(popupContent, {
+            maxWidth: "auto", 
+            minWidth: 300,
         });
 
       }
+
     }).addTo(map);
-  })
 
-
-/* fetch('villes_africaines_test.geojson')
-  .then(response => response.json())
-  .then(data => {
-    L.geoJSON(data, {
-      onEachFeature: function (feature, layer) {
-
-      if (feature.geometry && feature.geometry.type === 'Point') {
-          var latlng = [feature.geometry.coordinates[1], feature.geometry.coordinates[0]]; // [lat, lng]
-
-          var marker = layer.marker(latlng).addTo(map);
-
-          var popupContent = "<strong>" + feature.properties.name + "</strong><br>" 
-
-          marker.bindPopup(popupContent);
-        }
-      }
-    }).addTo(map);
-  })
-  .catch(error => {
-    console.error("Erreur lors du chargement du fichier GeoJSON:", error);
-  }); */
+})
 
 
 
 const tile = L.tileLayer(mapboxUrl, {
-  attribution: attr
+    attribution: attr
 }).addTo(map);
 
 
